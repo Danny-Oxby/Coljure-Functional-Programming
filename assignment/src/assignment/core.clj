407252e1250abcde2ce89619e85fb6853d08746e
DANNY OXBY <b9017792@hallam.shu.ac.uk>
2022-12-13 10:50:27 +0000

    updated the morsse code to use proper format

--- assignment/src/assignment/core.clj
+++ assignment/src/assignment/core.clj
@@ -1,6 +1,7 @@
 (ns assignment.core
   (:gen-class)) ; NEVER DELETE THIS
 (require '[clojure.string :as str]) ; used in the morse conversion
+(require '[clojure.spec.alpha :as s]) ; used in the spec
 
 (defn -main ; main is required entry point
   [& args])
@@ -13,8 +14,7 @@
                  "r" ".-." "s" "..." "t" "-" "u" "..-" "v" "...-" "w" ".--"
                  "x" "-..-" "y" "-.--" "z" "--.." "0" "-----" "1" ".----"
                  "2" "..---" "3" "...--" "4" "....-" "5" "....." "6" "-...."
-                 "7" "--..." "8" "---.." "9" "----." 
-                 " " "       " "       " " "; 1 space equals 7 in morse code
+                 "7" "--..." "8" "---.." "9" "----." ;; space is NOT handeled by the map
                  ".-" "a" "-..." "b" "-.-." "c" "-.." "d" "." "e" ;; morse values here
                  "..-." "f" "--." "g" "...." "h" ".." "i" ".---" "j" "-.-" "k"
                  ".-.." "l" "--" "m" "-." "n" "---" "o" ".--." "p" "--.-" "q"
@@ -28,18 +28,13 @@
     (if (= index (count input)) ; if the current index is the same as the input length return the convered string
       (apply str morsestr) ; return the morse string
       (recur ; loop begins here 
-        (inc index) ; increase the index for the next char in the string 
-        (if (= (+ index 1) (count input)) ; if is the last item
-          (concat morsestr (ConvertMap (subs (str/lower-case input) index (+ index 1)))) ; dont add the last space
-          (if (or (= (subs input index (+ index 1)) " ") (= (subs input (+ index 1) (+ index 2)) " ")) ; is this or the next value a space
-            (concat morsestr (ConvertMap (subs (str/lower-case input) index (+ index 1)))) ; id so don't add the extra 3 spaces
-            (concat morsestr (ConvertMap (subs (str/lower-case input) index (+ index 1))) "   ") ; three space between letters ; return the next index of the sting and the converted ascii value
-            )
-          )
-      )
-    ) 
-  )
-)
+       (inc index) ; increase the index for the next char in the string 
+       (if (= (+ index 1) (count input)) ; if is the last item
+         (concat morsestr (ConvertMap (subs (str/lower-case input) index (+ index 1)))) ; dont add the last space
+         (if (= (subs input index (+ index 1)) " ") ; is this or the next value a space
+           (concat morsestr "    ") ; convert the space to 4 (since its gets + 3 from the previouse letter to make 7 long)
+           (concat morsestr (ConvertMap (subs (str/lower-case input) index (+ index 1))) "   ") ; three space between letters ; return the next index of the sting and the converted ascii value
+           ))))))
 (println (ASCIIConvert "abb hi")) ; input string here .-   -...   -...       ....   ..
 
 ;; morse to ascii  (.-   -...   -...) -> "abb"
@@ -48,18 +43,24 @@
 ;; removed used part of the morse blacok (   -...   -...)
 ;; loop with new ascii value;
 (defn MorseConvert [input]
-    (loop [mosrsestr (str/split input #"   ") ; split the input on every space, the # begins the regex
-            output ""]
-            (if (= mosrsestr []) ; is the vector empty yet
-              (apply str output)
-              (recur ; loop it 
-               (subvec mosrsestr 1) ; remove the first item in the vector
-               (concat output (ConvertMap (first mosrsestr)))) ; convert the first item to ascii value and add to string 
-              )
-    )
-)
+  (loop [wordstr (str/split input #"       ") ; split the input on every 7/word space, the # begins the regex
+         asciistr ""]
+    (if (= wordstr [])
+      (apply str asciistr)
+      (recur
+       (subvec wordstr 1) ; remove the first item in the word vector
+       (concat asciistr
+               (loop [mosrsestr (str/split (first wordstr) #"   ") ; split the input on every 3/letter space, the # begins the regex
+                      output ""]
+                 (if (= mosrsestr []) ; is the vector empty yet
+                   (apply str output)
+                   (recur ; loop it 
+                    (subvec mosrsestr 1) ; remove the first item in the vector
+                    (concat output (ConvertMap (first mosrsestr)))) ; convert the first item to ascii value and add to string 
+                   ))" ") ; add sapce for word seperation
+       ))))
+(println (MorseConvert ".-   -...   -.-.   -..   .       .       .   .")) ; <- abcde e ee
 (println (MorseConvert ".-   -...   -.-.   -..   .       -...   .   -..")) ; <- abcde bed
-(println (str/split ".-   -...      -.-." #"   "))
 
 ;; weather data notes
 ;; in the format of Year Day Jan Feb March etc
--- assignment/test/assignment/core_test.clj
+++ assignment/test/assignment/core_test.clj
@@ -6,10 +6,20 @@
   (testing "FIXME, I fail."
     (is (= 1 1))))
 
-(deftest Ascii-converstion-test
+(deftest Ascii-conversion-test
   (testing "Ascii alphabet conversion"
      (is (= (ASCIIConvert "a") ".-")) ; <- mosrse for a
+     (is (= (ASCIIConvert "1") ".----")) ; <- mosrse for 1
      (is (not(= (ASCIIConvert "b") ".-")))
-     ;;(is (= (ASCIIConvert "hello world") ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."))
+     (is (= (ASCIIConvert "hello world") "....   .   .-..   .-..   ---       .--   ---   .-.   .-..   -.."))
    )
+  )
+
+(deftest Morse-conversion-test
+  (testing "Morse aplhabet conversion"
+    (is (= (MorseConvert "--..") "z"))
+    (is (= (MorseConvert "--..") "z"))
+    (is (not (= (MorseConvert ".-") "z")))
+    (is (= (MorseConvert "....   .   .-..   .-..   ---       .--   ---   .-.   .-..   -..") "hello world"))
+    )
   )
