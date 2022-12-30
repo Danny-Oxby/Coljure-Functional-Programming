1a675804d2af8eb958ef060d5c99c74041dd1f3d
DANNY OXBY <b9017792@hallam.shu.ac.uk>
2022-12-30 12:35:44 +0000

    finalproduct

--- assignment/src/assignment/core.clj
+++ assignment/src/assignment/core.clj
@@ -6,6 +6,11 @@
 (defn -main ; main is required entry point
   [& args])
 
+;; MENTION IN REPORT
+; For my morse and ascii I has used lowercase letters rather than upper case, this is becasue there is no
+; diffrence in morse between upper and lowercase, and morese normally only uses one case. In my case I used lower case
+; mainly since the converter i based of used lowercase
+
 ; morse code has 3 space "   " seperators between the letters and 7 space "       " decomes a "space" charactor
 (def ConvertMap {"a" ".-" "b" "-..." "c" "-.-." "d" "-.." "e" "." ;; ascii values here
                  "f" "..-." "g" "--." "h" "...." "i" ".." "j" ".---" "k" "-.-"
@@ -13,7 +18,7 @@
                  "r" ".-." "s" "..." "t" "-" "u" "..-" "v" "...-" "w" ".--"
                  "x" "-..-" "y" "-.--" "z" "--.." "0" "-----" "1" ".----"
                  "2" "..---" "3" "...--" "4" "....-" "5" "....." "6" "-...."
-                 "7" "--..." "8" "---.." "9" "----." ;; space is NOT handeled by the map
+                 "7" "--..." "8" "---.." "9" "----." " " " ";; space is space in the map
                  ".-" "a" "-..." "b" "-.-." "c" "-.." "d" "." "e" ;; morse values here
                  "..-." "f" "--." "g" "...." "h" ".." "i" ".---" "j" "-.-" "k"
                  ".-.." "l" "--" "m" "-." "n" "---" "o" ".--." "p" "--.-" "q"
@@ -29,24 +34,34 @@
 (s/def ::MorseInput (s/and string?
                            #(re-matches #"^[.\- ]+$" %)))
 
-(defn ASCIIConvert [input] ; this methods assumes that any input can't end in a space (since i tmake no gramatical sence)
+(defn ASCIIConvert [input] ; this methods assumes that any output can't end in a space (since it makes no gramatical sence)
   {:pre [(s/valid? ::ASCIIInput input)] ; the input must always be a letter or number
    :post [(s/valid? ::MorseInput %)]} ; the output must always be a morse string
-  (loop [index 0 morsestr ""] ; store the current position in the input and the current output string
-    (if (= index (count input)) ; if the current index is the same as the input length return the convered string
+  (let [value (str/trim input)]
+    (loop [index 0 morsestr ""] ; store the current position in the input and the current output string
+    (if (= index (count value)) ; if the current index is the same as the input length return the convered string
       (str/trim (apply str morsestr)) ; return the morse string <- removing the trailing spaces
       (recur ; loop begins here 
        (inc index) ; increase the index for the next char in the string 
-       (if (= (subs input index (+ index 1)) " ") ; is this value a space
-         (concat morsestr "    ") ; convert the space to 4 (since its gets + 3 from the previouse letter to make 7 long)
-         (concat morsestr (ConvertMap (subs input index (+ index 1))) "   ") ; three space between letters ; return the next index of the sting and the converted ascii value
-))))) ; the str/lower-case is un-needed here because of the :pre spec
+       (concat morsestr (ConvertMap (subs value index (+ index 1))) "   "))))))
 
-; (println (s/assert ::ASCIIInput "AbB Hi")) ; why is this valid?
-; (println (s/conform ::ASCIIInput "AbB Hi")) ; why dosn't conform work in pre conditions
 ; (println (ASCIIConvert "ab"))
-; (println (ASCIIConvert "abb hi")) ; input string here .-   -...   -...       ....   ..
+; (println (ASCIIConvert "john went to the game"))
+; (println (ASCIIConvert "  abb hi ")) ; input string here .-   -...   -...       ....   ..
 ; (println (ASCIIConvert "AbB Hi")) ; input string here should fail
+
+; (defn ASCIIConvert [input] ; this methods assumes that any output can't end in a space (since i tmake no gramatical sence)
+;   {:pre [(s/valid? ::ASCIIInput input)] ; the input must always be a letter or number
+;    :post [(s/valid? ::MorseInput %)]} ; the output must always be a morse string
+;   (let [values (mapv #(ConvertMap (str %)) input)] ; convert the input to a list
+;     (loop [morsestr "" asciilist values]
+;       (if (= (count asciilist) 0)
+;         (str/trim (apply str morsestr)) ; return the list as a sting and remove trailing spaces
+;         (recur
+;          (concat morsestr (first asciilist) "   ") ; ass the 3 "space" word space
+;          (rest asciilist)))))) ; shortern the list
+; (println (ASCIIConvert "abc de"))
+; (println (ConvertMap (str (first "abcde"))))
 
 ;; morse to ascii  (.-   -...   -...) -> "abb"
 ;; find the morse block (either the next " " of the end of input) <- split string at
@@ -56,7 +71,7 @@
 (defn MorseConvert [input]
   {:pre [(s/valid? ::MorseInput input)] ; the input must always be a morse value
    :post [(s/valid? ::ASCIIInput %)]} ; the output must always be a string letter or number
-  (loop [wordstr (str/split input #"       ") ; split the input on every 7/word space, the # begins the regex
+  (loop [wordstr (str/split (str/trim input) #"       ") ; split the input on every 7/word space, the # begins the regex
          asciistr ""]
     (if (= wordstr [])
       (apply str asciistr)
@@ -75,7 +90,7 @@
                  "" ; dont add a trailing space (for gramatical reasons)
                  " ")) ; add sapce for word seperation
        ))))
-; (println (MorseConvert ".-   -...   -.-.   -..   .       .       .   .")) ; <- abcde e ee
+; (println (MorseConvert "   .-   -...   -.-.   -..   .       .       .   . ")) ; <- abcde e ee
 ; (println (MorseConvert ".-   -...   -.-.   -..   .       -...   .   -..")) ; <- abcde bed
 ; (println (MorseConvert "abc")) ; <- should fail
 
@@ -101,8 +116,8 @@
 (def MonthList ["Jan" "Feb" "Mar" "Arp" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec"])
 ;; My specs use a matching name and the req-un to allow for the (conform (-> record)) method set to work
 (s/def ::Year (s/and integer?
-                      #(< 0001 %)
-                      #(> 9999 %)))
+                     #(< 0001 %)
+                     #(> 9999 %)))
 (s/def ::Month string?)
 (s/def ::DayList (s/coll-of double? :kind sequence))
 (s/def ::Monthlyweatherdata
@@ -111,7 +126,9 @@
 (s/def ::ListofYearWeatherData (s/coll-of ::YearWeatherData)) ; a list of valid YearWeatherData <-highest leve spec needed for most pre conditions
 
 (defn GetDailyData [year monthindex] ; conform seq? returned
-  {:pre [(s/valid? number? monthindex)]}
+  {:pre [(s/valid? number? monthindex)
+         (s/valid? (s/coll-of string?) year)]
+   :post [(s/valid? ::DayList %)]}
   (loop [datarow year values []]
     (if (= (count datarow) 0) ; for all 31 data peices
       (s/conform ::DayList values) ; make sure the return is a list of doubles
@@ -120,10 +137,12 @@
        (conj values ; add the current value to the list
              (double (/ (Integer. (str/trim ; remove the leading spaces and convert to integer, then to number else I get issues
                                    (subs (first datarow) (+ 11 (* 5 monthindex)) (+ 15 (* 5 monthindex))))) 10))))))) ; for each column (11 is the first data column)
-;(println (GetDailyData (str/split (slurp "oneyeardata.txt") #"\r\n") 0))
+; (println (GetDailyData (str/split (slurp "oneyeardata.txt") #"\r\n") 0))
+; (println (GetDailyData 123 0))
 
 (defn GetMonthData [year]
-  {:post [(s/valid? ::YearWeatherData %)]}
+  {:pre [(s/valid? (s/coll-of string?) year)]
+   :post [(s/valid? ::YearWeatherData %)]}
   (loop [monthindex 0 valuedata []]
     (if (= monthindex 12) ; for all 12 months
       valuedata
@@ -131,34 +150,36 @@
        (inc monthindex)
        (conj valuedata
              (s/conform ::Monthlyweatherdata (->Monthlyweatherdata ; create the record
-              (Integer. (str/trim (subs (first year) 0 5))) ;find the year by looking at the firs column of the 31 inputs
-              (nth MonthList monthindex) ; get the month by converting the index to the monthList
-              (GetDailyData year monthindex)))
-             )))))
+                                              (Integer. (str/trim (subs (first year) 0 5))) ;find the year by looking at the firs column of the 31 inputs
+                                              (nth MonthList monthindex) ; get the month by converting the index to the monthList
+                                              (GetDailyData year monthindex))))))))
 
 ; (println (GetMonthData (str/split (slurp "oneyeardata.txt") #"\r\n")))
+; (println (GetMonthData 123))
 
 (defn ReadYearlyColumn [filename]
-  {:post [(s/valid? ::ListofYearWeatherData %)]}
+  {:pre [(s/valid? string? filename)]
+   :post [(s/valid? ::ListofYearWeatherData %)]}
   (let [info (partition 31 (str/split (slurp filename) #"\r\n")) ; split on every line remove the trailling whate space
         yearvalue (loop [year info values []]
                     (if (= (count year) 0)
                       values
                       (recur
                        (rest year)
-                       (conj values (s/conform ::YearWeatherData 
-                             (GetMonthData (first year)))))))]
+                       (conj values (s/conform ::YearWeatherData
+                                               (GetMonthData (first year)))))))]
     yearvalue) ; output
   )
 ; (println (ReadYearlyColumn "weatherdata.txt"))
 ; (println (ReadYearlyColumn "fiveyeardata.txt"))
+; (println (ReadYearlyColumn 123))
 
 (defn Filter99 [list] ; extracted filter function since it's used in multiple places
   {:pre [(s/valid? ::DayList list)]} ; only pass in a list of days
   (filter (fn [x] ; add the new value to the current total
             (not (= -99.9 x))) ; ignore the invalid values
           list))
-
+; (println (Filter99 ["a" "b" "c"]))
 ; (println (Filter99 [4.7774196 3.8129032 3.4935484 4.4612904 4.387097 2.6451614 6.067742 3.0709677 3.248387 5.351613 2.7774193 2.6903226 0.29032257 2.7741935 2.8 3.7548387 -0.29677418 6.0580645 4.287097 1.0935484 4.3483872 5.3 3.667742 6.632258 -0.2548387 4.822581 1.5225806 1.2870967 3.2903225 1.4612904 3.5677419 4.3645163 2.083871 3.6 6.8 1.8903226 2.216129 4.112903 3.5677419 3.135484 1.7064517 2.8129032 4.3 2.3064516 3.083871 2.4870968 3.6193547 1.383871 4.716129 6.416129 1.6032258 4.819355 5.0935483 4.6 5.7967744 6.883871 7.4258065 1.4096774 1.8032258 5.783871 5.2193546 6.8774195 5.612903 3.0870967 4.1032257 5.303226 4.0225806 3.6903226 1.2870967 4.4129033 7.216129 7.4064517 0.39354845064516 6.283871 3.6806452 5.822581 6.148387 3.4290323 7.1870966 2.8806453 0.61612904 3.5709677 5.3290324 5.3096776 -0.19354838 4.1903224 6.0064516 4.6580644 -0.30322582 0.7 5.0903225 3.9064517 3.903226 4.596774 4.3967743 3.7064517 1.8935484 2.596774 4.8967743 3.2935483 -0.8 4.096774 1.8129033 4.806452 5.1 3.8935485 3.8935485 4.693548 7.3 2.1903226 7.2032256 3.403226 4.6 3.2935483 3.7064517 4.8967743 3.0032258 4.616129 3.903226 3.9064517 6.403226 6.2064514 6.693548 5.1064515 4.6032257 5.3 1.9032258 2.3 6.9064517 5.4903226 4.196774 6.5064516 5.803226 3.8064516 6.7967744 2.7806451 4.180645 2.1064515 3.3967743 5.8096776 4.2967744 5.306452 5.6903224 1.5903226 8.093549 2.73 6.803226 5.4 5.6903224 4.5 4.7032256 5.996774 3.8967743 2.2032259 1.8 2.6032257 3.596774 4.696774 5.4935484 4.196774 3.0 3.2935483 4.3 6.6064515 5.7935486 4.903226 8.0903225 5.2580647 2.016129 6.0870967 3.9451613 5.822581 5.6096773 0.29677418 4.383871 5.5741935 5.248387 6.306452 6.245161 5.596774 7.4774194 4.870968 4.303226 4.6580644 3.564516 5.5096774 6.419355 2.2774193 2.919355 5.774194 5.5387096 4.9741936 5.8096776 3.564516 5.709677 4.770968 5.383871 4.435484 6.470968 4.935484 3.5258064 3.1032257 -0.6935484 5.9580646 4.783871 6.3290324 5.148387 9.674193 5.9741936 4.7419353 6.8935485 5.787097 4.9741936 6.3645163 -99.9]))
 
 ;;;; //////////////////////////////////// Question 2.1 //////////////////////////////////////////
@@ -203,11 +224,12 @@
 ; (println (FindWarmestInMonth (ReadYearlyColumn "weatherdata.txt")))
 ; (println (.indexOf (:DayList (nth (first (ReadYearlyColumn "weatherdata.txt")) 0)) 6.2 ) )
 ; (println (FindHottestDay (ReadYearlyColumn "weatherdata.txt") 11))
+; (println (FindHottestDay 123 11))
 
 ;;;; //////////////////////////////////// Question 2.2 //////////////////////////////////////////
 
 (defrecord YearData [Year Value])
-(s/def ::YearData 
+(s/def ::YearData
   (s/keys :req-un [::Year ::Value]))
 (defrecord TempData [Warmest Coldest])
 (s/def ::Warmest ::YearData)
@@ -221,8 +243,8 @@
   (loop [monthindex 0 currentavg 0 stop false] ; stop is for 2022 when all data is invalid
     (if (or (= monthindex 12) stop)
       (s/conform ::Value (float (/ currentavg (if stop
-                             (- monthindex 1)
-                             monthindex))))
+                                                (- monthindex 1)
+                                                monthindex))))
       (let [listval (Filter99 (:DayList (nth yearinput monthindex)))]
         (recur ; for every month
          (inc monthindex)
@@ -235,6 +257,7 @@
            false)))))) ; divide the months total with the number of valid days
 ; (println (FindYearAvg (GetMonthData (str/split (slurp "oneyeardata.txt") #"\r\n"))))
 ; (println (FindYearAvg (GetMonthData (str/split (slurp "lastyeardata.txt") #"\r\n"))))
+; (println (FindYearAvg 123))
 ; (println (FindYearAvg (nth (ReadYearlyColumn "fiveyeardata.txt") 0))) ; 9.149081
 ; (println (FindYearAvg (nth (ReadYearlyColumn "fiveyeardata.txt") 1))) ; 9.24301
 ; (println (FindYearAvg (nth (ReadYearlyColumn "fiveyeardata.txt") 2))) ; 9.052168
@@ -243,7 +266,7 @@
 ; (println (FindYearAvg (nth (ReadYearlyColumn "fiveyeardata.txt") 5))) ; 9.067841
 
 (defn FindAvgWarmestAndColdestYears [datainput] ; find the average of each year's temp, ignoring -99.9, and sees if its the coldest or warmest year 
-    {:pre [(s/valid? ::ListofYearWeatherData datainput)]
+  {:pre [(s/valid? ::ListofYearWeatherData datainput)]
    :post [(s/valid? ::TempData %)]}
   (loop [yearlist datainput
          WarmestData (YearData. 0001 -99.9) ; the warmest possible value
@@ -287,24 +310,24 @@
 (s/def ::Nearest ::YearData) ; match the names so that the conform check is quicker
 (s/def ::Furthest ::YearData)
 (s/def ::MonthlyMeanAndVarience
-  (s/keys :req-un[::Month ::Mean ::Nearest ::Furthest]))
+  (s/keys :req-un [::Month ::Mean ::Nearest ::Furthest]))
 (s/def ::ListofMonthlyMeanAndVarience (s/coll-of ::MonthlyMeanAndVarience))
 ;(defrecord YearData [Year Value]) << already exists this is a reminder
 
 (defn MonthlyTempVariation [datainput monthindex targetavg] ; find the closes and farthers temp avg from the target
-    {:pre [(s/valid? ::ListofYearWeatherData datainput)
-           (s/valid? number? monthindex)
-           (s/valid? ::Value targetavg)]     
-     :post [(s/valid? ::MonthlyMeanAndVarience %)]}
+  {:pre [(s/valid? ::ListofYearWeatherData datainput)
+         (s/valid? number? monthindex)
+         (s/valid? ::Value targetavg)]
+   :post [(s/valid? ::MonthlyMeanAndVarience %)]}
   (loop [yearlist datainput avgvalues []]
     (let [listval (Filter99 (:DayList (nth (first yearlist) monthindex)))] ; remove the invalid -99.9 from the daylist
       (if (= yearlist []) ; for all of the years
         ;avgvalues
         (s/conform ::MonthlyMeanAndVarience (->MonthlyMeanAndVarience
-         (MonthList monthindex)
-         targetavg ; code snippit for abs found from https://groups.google.com/g/clojure/c/quEzEM_ndCY
-         (apply min-key #(abs (- (:Value %) targetavg)) avgvalues) ; search for (:Value %) since only models with value list will enter this part
-         (apply max-key #(abs (- (:Value %) targetavg)) avgvalues))) ; remove any -99.9 added in the recur
+                                             (MonthList monthindex)
+                                             targetavg ; code snippit for abs found from https://groups.google.com/g/clojure/c/quEzEM_ndCY
+                                             (apply min-key #(abs (- (:Value %) targetavg)) avgvalues) ; search for (:Value %) since only models with value list will enter this part
+                                             (apply max-key #(abs (- (:Value %) targetavg)) avgvalues))) ; remove any -99.9 added in the recur
         (recur
          (rest yearlist)
          (if (= listval [])
@@ -505,3 +528,33 @@
 ;                        (Simplerec. 2 "b")]]
 ;            (s/conform ::SimpleColl values))
 ; )
+
+; (def intlist [])
+; (defn createlist [length]
+;   (loop [index 0 locallist []]
+;     (if (= index length)
+;       (println (into intlist locallist))
+;       (recur
+;        (inc index)
+;        (conj locallist index)))))
+; (createlist 5)
+; (println intlist)
+
+
+; old version
+; (defn ASCIIConvert [input] ; this methods assumes that any input can't end in a space (since i tmake no gramatical sence)
+;   {:pre [(s/valid? ::ASCIIInput input)] ; the input must always be a letter or number
+;    :post [(s/valid? ::MorseInput %)]} ; the output must always be a morse string
+;   (loop [index 0 morsestr ""] ; store the current position in the input and the current output string
+;     (if (= index (count input)) ; if the current index is the same as the input length return the convered string
+;       (str/trim (apply str morsestr)) ; return the morse string <- removing the trailing spaces
+;       (recur ; loop begins here 
+;        (inc index) ; increase the index for the next char in the string 
+;        (if (= (subs input index (+ index 1)) " ") ; is this value a space
+;          (concat morsestr "    ") ; convert the space to 4 (since its gets + 3 from the previouse letter to make 7 long)
+;          (concat morsestr (ConvertMap (subs input index (+ index 1))) "   ") ; three space between letters ; return the next index of the sting and the converted ascii value
+; ))))) ; the str/lower-case is un-needed here because of the :pre spec
+
+; (defn pure-fucntion[a b]
+;   (* a b b))
+; (println (pure-fucntion 2 3))
